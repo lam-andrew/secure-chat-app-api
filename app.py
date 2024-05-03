bf12662a2e68c1f9ed2e045208153e4bb2721119
lam-andrew <andrewlam2193@gmail.com>
2024-05-02 22:32:11 -0700

    users and messages list resides on server side to maintain list even on UI refresh

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import eventlet
 eventlet.monkey_patch()
 
-from flask import Flask, request, send_from_directory
+from flask import Flask, jsonify, request, send_from_directory
 from flask_socketio import SocketIO,emit
 from flask_cors import CORS, cross_origin
 
@@ -10,11 +10,34 @@ CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*", logger=True, engineio_logger=True)
 
 users = {}
+messages = {}
 
 @app.route('/')
 @cross_origin()
 def serve():
     return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/users')
+def list_users():
+    """Endpoint to return a list of connected users."""
+    # Create a list of user details from the users dictionary
+    user_list = [{"sid": sid, "username": user_data['username'], "profilePicUrl": user_data['profilePicUrl']} for sid, user_data in users.items()]
+    return jsonify(user_list)
+
+@app.route('/messages')
+def list_messages():
+    """Endpoint to return a list of all messages."""
+    # Flatten the messages dictionary into a list of messages with session IDs
+    all_messages = []
+    for sid, msgs in messages.items():
+        for msg in msgs:
+            all_messages.append({
+                "sid": sid,
+                "username": msg["username"],
+                "profilePicUrl": msg["profilePicUrl"],
+                "text": msg["text"]
+            })
+    return jsonify(all_messages)
 
 # Socket Routes 
 @socketio.on('register')
@@ -27,6 +50,9 @@ def handle_register(data):
 @socketio.on('data')
 def handle_message(data):
     """event listener when client types a message"""
+    if request.sid not in messages:
+        messages[request.sid] = []
+    messages[request.sid].append(data)
     emit("data",{'data': data["text"],'sid': request.sid, 'username': data["username"], 'profilePicUrl': data["profilePicUrl"]},broadcast=True)
 
 @socketio.on("connect")
@@ -41,6 +67,11 @@ def disconnected():
     username = user_info.get('username', 'A user')
     print(f"user {request.sid} ({username}) disconnected")
     emit("userDisconnected", {'username': username}, broadcast=True)
+
+    # If there are no more users connected, clear the messages
+    if not users:  # Check if the users dictionary is empty
+        messages.clear()  # Empty the messages dictionary
+        print("All users have disconnected, clearing all messages.")
 
 
 if __name__ == "__main__":
